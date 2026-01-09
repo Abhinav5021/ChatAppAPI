2a75476ec49724d17fa291049b1f061dd3615aa6
Swapnil Supekar <swpnilsupekar@gmail.com>
2026-01-09 23:20:55 +0530

    create file

--- src/main/java/com/example/demo/user.java
+++ src/main/java/com/example/demo/user.java
@@ -2,4 +2,34 @@ package com.example.demo;
 
 public class user {
 
+	String user_name;
+	String password;
+	String phone_number;
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone_number() {
+		return phone_number;
+	}
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+	@Override
+	public String toString() {
+		return "user [user_name=" + user_name + ", password=" + password + ", phone_number=" + phone_number + "]";
+	}
+	
+	
+    
+
+	
 }
